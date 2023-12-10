633bf898fdae6f1620e17d4886fabca7a9b70e27
ryximu233 <ryximu@qq.com>
2023-12-11 00:11:44 +0800

    update

--- blossom-framework/blossom-spring-boot-starter-banner/src/main/java/cn/econets/blossom/framework/banner/config/BlossomBannerAutoConfiguration.java
+++ blossom-framework/blossom-spring-boot-starter-banner/src/main/java/cn/econets/blossom/framework/banner/config/BlossomBannerAutoConfiguration.java
@@ -1,13 +1,12 @@
 package cn.econets.blossom.framework.banner.config;
 
-import cn.econets.blossom.framework.banner.BannerApplicationRunner;
+import cn.econets.blossom.framework.banner.core.BannerApplicationRunner;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 /**
  * Banner 的自动配置类
  *
- * @author 芋道源码
  */
 @AutoConfiguration
 public class BlossomBannerAutoConfiguration {
