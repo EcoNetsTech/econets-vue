25104b26e1776a8413c79192b1eb53dab62e9724
ryximu233 <ryximu@qq.com>
2024-01-03 23:38:19 +0800

    【修复】CacheUtils 异步缓存的 ThreadLocal 变量传递

--- blossom-framework/blossom-common/src/main/java/cn/econets/blossom/framework/common/util/cache/CacheUtils.java
+++ blossom-framework/blossom-common/src/main/java/cn/econets/blossom/framework/common/util/cache/CacheUtils.java
@@ -1,10 +1,12 @@
 package cn.econets.blossom.framework.common.util.cache;
 
+import com.alibaba.ttl.threadpool.TtlExecutors;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 
 import java.time.Duration;
+import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 /**
@@ -14,11 +16,13 @@ import java.util.concurrent.Executors;
 public class CacheUtils {
 
     public static <K, V> LoadingCache<K, V> buildAsyncReloadingCache(Duration duration, CacheLoader<K, V> loader) {
+        Executor executor = Executors.newCachedThreadPool( TtlExecutors.getDefaultDisableInheritableThreadFactory()); // TTL 保证 ThreadLocal 可以透传
+
         return CacheBuilder.newBuilder()
                 // 只阻塞当前数据加载线程，其他线程返回旧值
                 .refreshAfterWrite(duration)
                 // 通过 asyncReloading 实现全异步加载，包括 refreshAfterWrite 被阻塞的加载线程
-                .build(CacheLoader.asyncReloading(loader, Executors.newCachedThreadPool())); // TODO 可能要思考下，未来要不要做成可配置
+                .build(CacheLoader.asyncReloading(loader, executor));
     }
 
 }
